924a3052cbf891ca7f85d5ec1036ad08d3ed1ad9
Jack skehan <jackskehan@swordmedia.ie>
2018-10-22 10:52:32 +0100

    product destroy

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 use App\Http\Resources\Product\ProductResource;
 use App\Http\Resources\Product\ProductCollection;
 use App\Http\Requests\ProductRequest;
-use \Illuminate\Http\Response;
+use Symfony\Component\HttpFoundation\Response;
 class ProductController extends Controller
 {
 
@@ -15,98 +15,99 @@ class ProductController extends Controller
   public function __construct(){
     $this->middleware('auth:api')->except('index','show');
   }
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-      //if you want them to be the same as productResource
-        // return ProductResource::collection(Product::all());
-                 return ProductCollection::collection(Product::paginate(30));
-    }
+  /**
+  * Display a listing of the resource.
+  *
+  * @return \Illuminate\Http\Response
+  */
+  public function index()
+  {
+    //if you want them to be the same as productResource
+    // return ProductResource::collection(Product::all());
+    return ProductCollection::collection(Product::paginate(30));
+  }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+  /**
+  * Show the form for creating a new resource.
+  *
+  * @return \Illuminate\Http\Response
+  */
+  public function create()
+  {
+    //
+  }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(ProductRequest $request)
-    {
-        $product = new Product;
-        $product->name = $request->name;
-        $product->detail = $request->description;
-        $product->price = $request->price;
-        $product->stock = $request->stock;
-        $product->discount = $request->discount;
+  /**
+  * Store a newly created resource in storage.
+  *
+  * @param  \Illuminate\Http\Request  $request
+  * @return \Illuminate\Http\Response
+  */
+  public function store(ProductRequest $request)
+  {
+    $product = new Product;
+    $product->name = $request->name;
+    $product->detail = $request->description;
+    $product->price = $request->price;
+    $product->stock = $request->stock;
+    $product->discount = $request->discount;
 
-        $product->save();
+    $product->save();
 
-        return response([ 'data' => new ProductResource($product) ],Response::HTTP_CREATED);
-    }
+    return response([ 'data' => new ProductResource($product) ],Response::HTTP_CREATED);
+  }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Model\Product  $product
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Product $product)
-    {
-      //without resource restrictions
-        // return $product;
-        return new ProductResource( $product);
-    }
+  /**
+  * Display the specified resource.
+  *
+  * @param  \App\Model\Product  $product
+  * @return \Illuminate\Http\Response
+  */
+  public function show(Product $product)
+  {
+    //without resource restrictions
+    // return $product;
+    return new ProductResource( $product);
+  }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Model\Product  $product
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Product $product)
-    {
-        //
-    }
+  /**
+  * Show the form for editing the specified resource.
+  *
+  * @param  \App\Model\Product  $product
+  * @return \Illuminate\Http\Response
+  */
+  public function edit(Product $product)
+  {
+    //
+  }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Model\Product  $product
-     * @return \Illuminate\Http\Response
-     */
-    public function update(ProductRequest $request, Product $product)
-    {
-      //making description = detail
-      $request['detail'] =$request->description;
-      //unsetting description to no llonger be there
-      unset($request['description']);
-      $product->update($request->all());
+  /**
+  * Update the specified resource in storage.
+  *
+  * @param  \Illuminate\Http\Request  $request
+  * @param  \App\Model\Product  $product
+  * @return \Illuminate\Http\Response
+  */
+  public function update(Request $request, Product $product)
+  {
+    //making description = detail
+    $request['detail'] =$request->description;
+    //unsetting description to no llonger be there
+    unset($request['description']);
+    $product->update($request->all());
 
-        return response([ 'data' => new ProductResource($product) ],Response::HTTP_CREATED);
-    }
+    return response([ 'data' => new ProductResource($product) ],Response::HTTP_CREATED);
+  }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Model\Product  $product
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Product $product)
-    {
-        //
-    }
+  /**
+  * Remove the specified resource from storage.
+  *
+  * @param  \App\Model\Product  $product
+  * @return \Illuminate\Http\Response
+  */
+  public function destroy(Product $product)
+  {
+    $product->delete();
+    return response(null ,Response::HTTP_NO_CONTENT);
+  }
 }
